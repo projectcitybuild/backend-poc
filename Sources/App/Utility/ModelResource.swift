db5cd2cd5869f9e4649391bad68298d6d271fd8b
andyksaw <andy-saw@hotmail.com>
2020-03-06 23:43:16 +0900

    Add documentation for ModelResource

--- Sources/App/Utility/ModelResource.swift
+++ Sources/App/Utility/ModelResource.swift
@@ -8,12 +8,17 @@
 import Vapor
 import Fluent
 
+/// Represents a public-facing model that our internal models can map into when displayed
+/// in a response. By mapping our models into resources, our internal models can change
+/// without causing breaking changes for users of our public API
 protocol ModelResource: Content {
 
     associatedtype RawModel: Model
     init(rawModel model: RawModel)
 }
 
+/// Provides a convenience method to automatically map the current model into its corresponding,
+/// public-facing resource model
 protocol ModelResourceConvertible: Model {
 
     associatedtype Resource: ModelResource
